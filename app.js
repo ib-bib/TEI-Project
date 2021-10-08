271ca262d07f32a3b88d23685a2ba43811c5a560
ib-bib <81387641+ib-bib@users.noreply.github.com>
2021-10-08 17:01:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,18 @@
-const darkThemeButton = document.getElementById("toggleDark");
-const statsButton = document.getElementById("getStats");
-const ttsButton = document.getElementById("toggleTTS");
+const darkToggle = document.getElementById("toggleDark");
+// const 
 
-document.body.addEventListener("keyup", (ev) => {
-  
-    if (ev.key === "d") {
-      darkThemeButton.focus();
+let navIcons = document.getElementsByClassName("navIcon");
+
+darkToggle.addEventListener("click", ()=> {
+    for (let i = 0; i < navIcons.length; i++) {
+        navIcons[i].classList.toggle("dark");
     }
 
-    else if (ev.key === "s") {
-      statsButton.focus();
+    if (document.body.className === "bootstrap") {
+        document.body.className = "bootstrap-dark";
+    } else {
+        document.body.className = "bootstrap";
     }
 
-    else if (ev.key === "t") {
-      ttsButton.focus();
-    }
-
+    // darkToggle.setAttribute("src")
 });
-
-darkThemeButton.addEventListener('click', () => {
-    darkThemeButton.style.transform = "rotate(180deg)";
-    darkThemeButton.style.opacity = 0;
-});
-
-darkThemeButton.addEventListener('keyup' , (ev) => {
-  if (ev.key === ' ' || ev.key === 'Enter') {
-    ev.preventDefault();
-    darkThemeButton.click();
-  }
-});
-
-var docWidth = document.documentElement.offsetWidth;
-
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
-  }
-);
